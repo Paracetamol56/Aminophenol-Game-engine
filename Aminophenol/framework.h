fd7c89c43d3c496ae6db5f3b696b42ae7ded72e8
Mathéo <matheo.galu56@gmail.com>
2022-11-28 19:46:46 +0100

    Make VS project structure

---
+++ Aminophenol/framework.h
@@ -0,0 +1,29 @@
+
+#ifndef FRAMEWORK_H
+#define FRAMEWORK_H
+
+// Remove unusual windows stuff
+#define WIN32_LEAN_AND_MEAN
+
+// Define the export macro
+#ifdef AMINOPHENOL_EXPORTS
+#define AMINOPHENOL_API __declspec(dllexport)
+#else
+#define AMINOPHENOL_API __declspec(dllimport)
+#endif
+
+// C/C++ headers
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <memory>
+
+// Vulkan headers
+#include <vulkan/vulkan.hpp>
+
+// GLFW headers
+#include <GLFW/glfw3.h>
+
+#endif // FRAMEWORK_H
