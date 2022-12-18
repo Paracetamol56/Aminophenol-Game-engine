9b0f1575c9a8edcb726232bfa38a6000ca48d8a7
Mathéo <matheo.galu56@gmail.com>
2022-12-18 13:02:03 +0100

    Vector2 implementation (tested)

--- Aminophenol/Maths/Vector2.h
+++ Aminophenol/Maths/Vector2.h
@@ -13,7 +13,7 @@ namespace Aminophenol::Maths
 	public:
 		
 		/// <summary>
-		/// Basic constructor.
+		/// Empty constructor.
 		/// Initialize the vector to (0, 0).
 		/// </summary>
 		Vector2();
@@ -31,7 +31,256 @@ namespace Aminophenol::Maths
 		/// </summary>
 		/// <param name="x">The x value to initialize the vector to.</param>
 		/// <param name="y">The y value to initialize the vector to.</param>
-		Vector2(const T& x, const T& y);
+		Vector2(const T& xValue, const T& yValue);
+
+		/// <summary>
+		/// Two values constructor from a different type.
+		/// Initialize the vector to (x, y).
+		/// </summary>
+		/// <param name="x">The x value to initialize the vector to.</param>
+		/// <param name="y">The y value to initialize the vector to.</param>
+		template<typename U, typename V>
+		Vector2(const U& xValue, const V& yValue);
+
+		/// <summary>
+		/// Copy constructor.
+		/// </summary>
+		/// <param name="other">The vector to copy.</param>
+		Vector2(const Vector2<T>& other);
+
+		/// <summary>
+		/// Copy constructor from a different type.
+		/// </summary>
+		/// <param name="other">The vector to copy.</param>
+		template<typename U>
+		Vector2(const Vector2<U>& other);
+
+		/// <summary>
+		/// Move constructor.
+		/// </summary>
+		/// <param name="other">The vector to move.</param>
+		Vector2(Vector2<T>&& other);
+
+		/// <summary>
+		/// Copy constructor from a different type.
+		/// </summary>
+		/// <param name="other">The vector to copy.</param>
+		template<typename U>
+		Vector2(Vector2<U>&& other);
+		
+		/// <summary>
+		/// Assignment operator.
+		/// </summary>
+		/// <param name="other">The vector to assign.</param>
+		/// <returns>A reference to the vector.</returns>
+		Vector2<T>& operator=(const Vector2<T>& other);
+		
+		/// <summary>
+		/// Assignment operator from a different type.
+		/// </summary>
+		/// <param name="other">The vector to assign.</param>
+		/// <returns>A reference to the vector.</returns>
+		template<typename U>
+		Vector2<T>& operator=(const Vector2<U>& other);
+
+		/// <summary>
+		/// Move assignment operator.
+		/// </summary>
+		/// <param name="other">The vector to move.</param>
+		/// <returns>A reference to the vector.</returns>
+		Vector2<T>& operator=(Vector2<T>&& other);
+
+		/// <summary>
+		/// Move assignment operator from a different type.
+		/// </summary>
+		/// <param name="other">The vector to move.</param>
+		/// <returns>A reference to the vector.</returns>
+		template<typename U>
+		Vector2<T>& operator=(Vector2<U>&& other);
+		
+		/// <summary>
+		/// Addition method.
+		/// </summary>
+		/// <param name="other">The vector to add.</param>
+		/// <returns>The sum of the two vectors.</returns>
+		Vector2<T> add(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Subtraction method.
+		/// </summary>
+		/// <param name="other">The vector to subtract.</param>
+		/// <returns>The difference of the two vectors.</returns>
+		Vector2<T> subtract(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Multiplication method.
+		/// </summary>
+		/// <param name="scalar">The number to multiply by.</param>
+		/// <returns>The product of the vector and the number.</returns>
+		Vector2<T> multiply(const T& scalar) const;
+
+		/// <summary>
+		/// Division method.
+		/// </summary>
+		/// <param name="scalar">The number to divide by.</param>
+		/// <returns>The quotient of the vector and the number.</returns>
+		/// <exception cref="std::invalid_argument">Thrown if other is 0.</exception>
+		Vector2<T> divide(const T& scalar) const;
+		
+		/// <summary>
+		/// Addition operator.
+		/// </summary>
+		/// <param name="other">The vector to add.</param>
+		/// <returns>The result of the addition.</returns>
+		Vector2<T> operator+(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Subtraction operator.
+		/// </summary>
+		/// <param name="other">The vector to subtract.</param>
+		/// <returns>The result of the subtraction.</returns>
+		Vector2<T> operator-(const Vector2<T>& other) const;
+
+		/// <summary>
+		/// Multiplication operator.
+		/// </summary>
+		/// <param name="other">The number to multiply by.</param>
+		/// <returns>The result of the multiplication.</returns>
+		Vector2<T> operator*(const T& other) const;
+		
+		/// <summary>
+		/// Division operator.
+		/// </summary>
+		/// <param name="other">The number to divide by.</param>
+		/// <returns>The result of the division.</returns>
+		/// <exception cref="std::invalid_argument">Thrown if other is 0.</exception>
+		Vector2<T> operator/(const T& other) const;
+
+		/// <summary>
+		/// Addition assignment operator.
+		/// </summary>
+		/// <param name="other">The vector to add.</param>
+		/// <returns>A reference to the vector.</returns>
+		Vector2<T>& operator+=(const Vector2<T>& other);
+		
+		/// <summary>
+		/// Subtraction assignment operator.
+		/// </summary>
+		/// <param name="other">The vector to subtract.</param>
+		/// <returns>A reference to the vector.</returns>
+		Vector2<T>& operator-=(const Vector2<T>& other);
+		
+		/// <summary>
+		/// Multiplication assignment operator.
+		/// </summary>
+		/// <param name="other">The number to multiply by.</param>
+		/// <returns>A reference to the vector.</returns>
+		Vector2<T>& operator*=(const T& other);
+		
+		/// <summary>
+		/// Division assignment operator.
+		/// </summary>
+		/// <param name="other">The number to divide by.</param>
+		/// <returns>A reference to the vector.</returns>
+		/// <exception cref="std::invalid_argument">Thrown if other is 0.</exception>
+		Vector2<T>& operator/=(const T& other);
+
+		/// <summary>
+		/// Negation operator.
+		/// </summary>
+		/// <returns>The negated vector.</returns>
+		Vector2<T> operator-() const;
+
+		/// <summary>
+		/// Dot product method.
+		/// </summary>
+		/// <param name="other">The vector to dot product with.</param>
+		/// <returns>The dot product.</returns>
+		T dot(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Dot product operator.
+		/// </summary>
+		/// <param name="other">The vector to dot product with.</param>
+		/// <returns>The dot product.</returns>
+		T operator*(const Vector2<T>& other) const;
+
+		/// <summary>
+		/// Cross product method.
+		/// </summary>
+		/// <param name="other">The vector to cross product with.</param>
+		/// <returns>The cross product.</returns>
+		T cross(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Cross product operator.
+		/// </summary>
+		/// <param name="other">The vector to cross product with.</param>
+		/// <returns>The cross product.</returns>
+		T operator^(const Vector2<T>& other) const;
+
+		/// <summary>
+		/// Magnitude method.
+		/// </summary>
+		/// <returns>The magnitude.</returns>
+		T magnitude() const;
+		
+		/// <summary>
+		/// Normalization method.
+		/// </summary>
+		/// <returns>The normalized vector.</returns>
+		/// <exception cref="std::invalid_argument">Thrown if the magnitude is 0.</exception>
+		Vector2<T> normalize() const;
+		
+		/// <summary>
+		/// Equality operator.
+		/// </summary>
+		/// <param name="other">The vector to compare to.</param>
+		/// <returns>True if the vectors are equal, false otherwise.</returns>
+		bool operator==(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Inequality operator.
+		/// </summary>
+		/// <param name="other">The vector to compare to.</param>
+		/// <returns>True if the vectors are not equal, false otherwise.</returns>
+		bool operator!=(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Less than operator.
+		/// </summary>
+		/// <param name="other">The vector to compare to.</param>
+		/// <returns>True if the vector is less than the other vector, false otherwise.</returns>
+		bool operator<(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Greater than operator.
+		/// </summary>
+		/// <param name="other">The vector to compare to.</param>
+		/// <returns>True if the vector is greater than the other vector, false otherwise.</returns>
+		bool operator>(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Less than or equal to operator.
+		/// </summary>
+		/// <param name="other">The vector to compare to.</param>
+		/// <returns>True if the vector is less than or equal to the other vector, false otherwise.</returns>
+		bool operator<=(const Vector2<T>& other) const;
+
+		/// <summary>
+		/// Greater than or equal to operator.
+		/// </summary>
+		/// <param name="other">The vector to compare to.</param>
+		/// <returns>True if the vector is greater than or equal to the other vector, false otherwise.</returns>
+		bool operator>=(const Vector2<T>& other) const;
+		
+		/// <summary>
+		/// Output stream operator.
+		/// </summary>
+		/// <param name="stream">The stream to output to.</param>
+		/// <param name="vector">The vector to output.</param>
+		/// <returns>A reference to the stream.</returns>
+		friend std::ostream& operator<<(std::ostream& stream, const Vector2<T>& vector);
 		
 		T x;
 		T y;
@@ -42,23 +291,302 @@ namespace Aminophenol::Maths
 		
 	};
 	
+	// Empty constructor
 	template<typename T>
 	Vector2<T>::Vector2()
 		: x(0)
 		, y(0)
 	{}
 
+	// Single value constructor
 	template<typename T>
 	Vector2<T>::Vector2(const T& value)
 		: x(value)
 		, y(value)
 	{}
 
+	// Two value constructor
 	template<typename T>
 	Vector2<T>::Vector2(const T& xValue, const T& yValue)
 		: x(xValue)
 		, y(yValue)
 	{}
+	
+	// Two values constructor from a different type
+	template<typename T>
+	template<typename U, typename V>
+	Vector2<T>::Vector2(const U& xValue, const V& yValue)
+		: x(static_cast<T>(xValue))
+		, y(static_cast<T>(yValue))
+	{}
+	
+	// Copy constructor
+	template<typename T>
+	Vector2<T>::Vector2(const Vector2<T>& other)
+		: x(other.x)
+		, y(other.y)
+	{}
+
+	// Copy constructor from a different type
+	template<typename T>
+	template<typename U>
+	Vector2<T>::Vector2(const Vector2<U>& other)
+		: x(static_cast<T>(other.x))
+		, y(static_cast<T>(other.y))
+	{}
+	
+	// Move constructor
+	template<typename T>
+	Vector2<T>::Vector2(Vector2<T>&& other)
+		: x(std::move(other.x))
+		, y(std::move(other.y))
+	{}
+	
+	// Move constructor from a different type
+	template<typename T>
+	template<typename U>
+	Vector2<T>::Vector2(Vector2<U>&& other)
+		: x(static_cast<T>(std::move(other.x)))
+		, y(static_cast<T>(std::move(other.y)))
+	{}
+	
+	// Assignment operator
+	template<typename T>
+	Vector2<T>& Vector2<T>::operator=(const Vector2<T>& other)
+	{
+		x = other.x;
+		y = other.y;
+		return *this;
+	}
+
+	// Assignment operator from a different type
+	template<typename T>
+	template<typename U>
+	Vector2<T>& Vector2<T>::operator=(const Vector2<U>& other)
+	{
+		x = static_cast<T>(other.x);
+		y = static_cast<T>(other.y);
+		return *this;
+	}
+
+	// Move assignment operator
+	template<typename T>
+	Vector2<T>& Vector2<T>::operator=(Vector2<T>&& other)
+	{
+		x = std::move(other.x);
+		y = std::move(other.y);
+		return *this;
+	}
+	
+	// Move assignment operator from a different type
+	template<typename T>
+	template<typename U>
+	Vector2<T>& Vector2<T>::operator=(Vector2<U>&& other)
+	{
+		x = static_cast<T>(std::move(other.x));
+		y = static_cast<T>(std::move(other.y));
+		return *this;
+	}
+
+	// Addition method
+	template<typename T>
+	Vector2<T> Vector2<T>::add(const Vector2<T>& other) const
+	{
+		return Vector2<T>(x + other.x, y + other.y);
+	}
+	
+	// Subtraction method
+	template<typename T>
+	Vector2<T> Vector2<T>::subtract(const Vector2<T>& other) const
+	{
+		return Vector2<T>(x - other.x, y - other.y);
+	}
+	
+	// Multiplication method
+	template<typename T>
+	Vector2<T> Vector2<T>::multiply(const T& scalar) const
+	{
+		return Vector2<T>(x * scalar, y * scalar);
+	}
+
+	// Division method
+	template<typename T>
+	Vector2<T> Vector2<T>::divide(const T& scalar) const
+	{
+		if (scalar == 0)
+		{
+			throw std::invalid_argument("Cannot divide by zero.");
+		}
+		return Vector2<T>(x / scalar, y / scalar);
+	}
+
+	// Addition operator
+	template<typename T>
+	Vector2<T> Vector2<T>::operator+(const Vector2<T>& other) const
+	{
+		return add(other);
+	}
+
+	// Subtraction operator
+	template<typename T>
+	Vector2<T> Vector2<T>::operator-(const Vector2<T>& other) const
+	{
+		return subtract(other);
+	}
+
+	// Multiplication operator
+	template<typename T>
+	Vector2<T> Vector2<T>::operator*(const T& scalar) const
+	{
+		return multiply(scalar);
+	}
+	
+	// Division operator
+	template<typename T>
+	Vector2<T> Vector2<T>::operator/(const T& scalar) const
+	{
+		return divide(scalar);
+	}
+	
+	// Addition assignment operator
+	template<typename T>
+	Vector2<T>& Vector2<T>::operator+=(const Vector2<T>& other)
+	{
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+	
+	// Subtraction assignment operator
+	template<typename T>
+	Vector2<T>& Vector2<T>::operator-=(const Vector2<T>& other)
+	{
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
+	
+	// Multiplication assignment operator
+	template<typename T>
+	Vector2<T>& Vector2<T>::operator*=(const T& other)
+	{
+		x *= other;
+		y *= other;
+		return *this;
+	}
+	
+	// Division assignment operator
+	template<typename T>
+	Vector2<T>& Vector2<T>::operator/=(const T& other)
+	{
+		x /= other;
+		y /= other;
+		return *this;
+	}
+	
+	// Negation operator
+	template<typename T>
+	Vector2<T> Vector2<T>::operator-() const
+	{
+		return Vector2<T>(-x, -y);
+	}
+	
+	// Dot product method
+	template<typename T>
+	T Vector2<T>::dot(const Vector2<T>& other) const
+	{
+		return x * other.x + y * other.y;
+	}
+	
+	// Dot product operator
+	template<typename T>
+	T Vector2<T>::operator*(const Vector2<T>& other) const
+	{
+		return dot(other);
+	}
+
+	// Cross product method
+	template<typename T>
+	T Vector2<T>::cross(const Vector2<T>& other) const
+	{
+		return x * other.y - y * other.x;
+	}
+
+	// Cross product operator
+	template<typename T>
+	T Vector2<T>::operator^(const Vector2<T>& other) const
+	{
+		return cross(other);
+	}
+	
+	// Magnitude method
+	template<typename T>
+	T Vector2<T>::magnitude() const
+	{
+		return std::sqrt(x * x + y * y);
+	}
+	
+	// Normalization method
+	template<typename T>
+	Vector2<T> Vector2<T>::normalize() const
+	{
+		if (x == 0 && y == 0)
+		{
+			return Vector2<T>(0, 0);
+		}
+		T mag = magnitude();
+		return Vector2<T>(x / mag, y / mag);
+	}
+
+	// Equality operator
+	template<typename T>
+	bool Vector2<T>::operator==(const Vector2<T>& other) const
+	{
+		return x == other.x && y == other.y;
+	}
+
+	// Inequality operator
+	template<typename T>
+	bool Vector2<T>::operator!=(const Vector2<T>& other) const
+	{
+		return !(*this == other);
+	}
+
+	// Less than operator
+	template<typename T>
+	bool Vector2<T>::operator<(const Vector2<T>& other) const
+	{
+		return x < other.x&& y < other.y;
+	}
+
+	// Greater than operator
+	template<typename T>
+	bool Vector2<T>::operator>(const Vector2<T>& other) const
+	{
+		return x > other.x&& y > other.y;
+	}
+
+	// Less than or equal to operator
+	template<typename T>
+	bool Vector2<T>::operator<=(const Vector2<T>& other) const
+	{
+		return x <= other.x && y <= other.y;
+	}
+	
+	// Greater than or equal to operator
+	template<typename T>
+	bool Vector2<T>::operator>=(const Vector2<T>& other) const
+	{
+		return x >= other.x && y >= other.y;
+	}
+
+	// Output stream operator
+	template<typename T>
+	std::ostream& operator<<(std::ostream& stream, const Vector2<T>& vector)
+	{
+		stream << "Vector2(" << vector.x << ", " << vector.y << ")";
+		return stream;
+	}
 	
 }
 
--- TestAminophenol/Maths/TestVector2.cpp
+++ TestAminophenol/Maths/TestVector2.cpp
@@ -14,26 +14,292 @@ namespace TestAminophenol
 	{
 	public:
 		
+		// Test the default constructor
 		TEST_METHOD(emptyConstructor)
 		{
-			Aminophenol::Maths::Vector2<double> vector2;
-			Assert::AreEqual(vector2.x, 0.0);
-			Assert::AreEqual(vector2.y, 0.0);
+			Aminophenol::Maths::Vector2<double> v0;
+			Assert::AreEqual(v0.x, 0.0);
+			Assert::AreEqual(v0.y, 0.0);
 		}
 
+		// Test the single value constructor
 		TEST_METHOD(singleValueConstructor)
 		{
-			Aminophenol::Maths::Vector2<double> vector2(1.0);
-			Assert::AreEqual(vector2.x, 1.0);
-			Assert::AreEqual(vector2.y, 1.0);
+			Aminophenol::Maths::Vector2<double> v0(1.0);
+			Assert::AreEqual(v0.x, 1.0);
+			Assert::AreEqual(v0.y, 1.0);
 		}
 
+		// Test the two values constructor
 		TEST_METHOD(twoValuesConstructor)
 		{
-			Aminophenol::Maths::Vector2<double> vector2(1.0, 2.0);
-			Assert::AreEqual(vector2.x, 1.0);
-			Assert::AreEqual(vector2.y, 2.0);
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Assert::AreEqual(v0.x, 1.0);
+			Assert::AreEqual(v0.y, 2.0);
 		}
+
+		// Test with two different types
+		TEST_METHOD(twoDifferentTypesConstructor)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0f, 2);
+			Assert::AreEqual(v0.x, 1.0);
+			Assert::AreEqual(v0.y, 2.0);
+		}
+
+		// Test the copy constructor
+		TEST_METHOD(copyConstructor)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(v0);
+			Assert::AreEqual(v1.x, 1.0);
+			Assert::AreEqual(v1.y, 2.0);
+		}
+		
+		// Test the copy constructor from a different type
+		TEST_METHOD(copyConstructorDifferentType)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<int> v1(v0);
+			Assert::AreEqual(v1.x, 1);
+			Assert::AreEqual(v1.y, 2);
+		}
+		
+		// Test the copy constructor from a Vector3
+		//TEST_METHOD(copyConstructorVector3)
+		//{
+		//	Aminophenol::Maths::Vector3<double> v0(1.0, 2.0, 3.0);
+		//	Aminophenol::Maths::Vector2<double> v1(v0);
+		//	Assert::AreEqual(v1.x, 1.0);
+		//	Assert::AreEqual(v1.y, 2.0);
+		//}
+
+		// Test the copy constructor from a Vector4
+		//TEST_METHOD(copyConstructorVector4)
+		//{
+		//	Aminophenol::Maths::Vector4<double> v0(1.0, 2.0, 3.0, 4.0);
+		//	Aminophenol::Maths::Vector2<double> v1(v0);
+		//	Assert::AreEqual(v1.x, 1.0);
+		//	Assert::AreEqual(v1.y, 2.0);
+		//}
+
+		// Test the addition method
+		TEST_METHOD(additionMethod)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(3.0, 4.0);
+			Aminophenol::Maths::Vector2<double> v2 = v0.add(v1);
+			Assert::AreEqual(v2.x, 4.0);
+			Assert::AreEqual(v2.y, 6.0);
+		}
+		
+		// Test the addition operator
+		TEST_METHOD(additionOperator)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(3.0, 4.0);
+			Aminophenol::Maths::Vector2<double> v2 = v0 + v1;
+			Assert::AreEqual(v2.x, 4.0);
+			Assert::AreEqual(v2.y, 6.0);
+		}
+		
+		// Test the addition operator with a different type
+		TEST_METHOD(additionOperatorDifferentType)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<int> v1(3, 4);
+			Aminophenol::Maths::Vector2<double> v2 = v0 + v1;
+			Assert::AreEqual(v2.x, 4.0);
+			Assert::AreEqual(v2.y, 6.0);
+		}
+
+		// Test the substraction method
+		TEST_METHOD(substractionMethod)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(3.0, 4.0);
+			Aminophenol::Maths::Vector2<double> v2 = v0.subtract(v1);
+			Assert::AreEqual(v2.x, -2.0);
+			Assert::AreEqual(v2.y, -2.0);
+		}
+
+		// Test the substraction operator
+		TEST_METHOD(substractionOperator)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(3.0, 4.0);
+			Aminophenol::Maths::Vector2<double> v2 = v0 - v1;
+			Assert::AreEqual(v2.x, -2.0);
+			Assert::AreEqual(v2.y, -2.0);
+		}
+		
+		// Test the substraction operator with a different type
+		TEST_METHOD(substractionOperatorDifferentType)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<int> v1(3, 4);
+			Aminophenol::Maths::Vector2<double> v2 = v0 - v1;
+			Assert::AreEqual(v2.x, -2.0);
+			Assert::AreEqual(v2.y, -2.0);
+		}
+
+		// Test the multiplication method
+		TEST_METHOD(multiplicationMethod)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0.multiply(2.0);
+			Assert::AreEqual(v1.x, 2.0);
+			Assert::AreEqual(v1.y, 4.0);
+		}
+
+		// Test the multiplication operator
+		TEST_METHOD(multiplicationOperator)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0 * 2.0;
+			Assert::AreEqual(v1.x, 2.0);
+			Assert::AreEqual(v1.y, 4.0);
+		}
+
+		// Test the multiplication operator with a different type
+		TEST_METHOD(multiplicationOperatorDifferentType)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0 * 2;
+			Assert::AreEqual(v1.x, 2.0);
+			Assert::AreEqual(v1.y, 4.0);
+		}
+
+		// Test the multiplication by 0
+		TEST_METHOD(multiplicationByZero)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0.multiply(0.0);
+			Assert::AreEqual(v1.x, 0.0);
+			Assert::AreEqual(v1.y, 0.0);
+		}
+		
+		// Test the division method
+		TEST_METHOD(divisionMethod)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0.divide(2.0);
+			Assert::AreEqual(v1.x, 0.5);
+			Assert::AreEqual(v1.y, 1.0);
+		}
+		
+		// Test the division operator
+		TEST_METHOD(divisionOperator)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0 / 2.0;
+			Assert::AreEqual(v1.x, 0.5);
+			Assert::AreEqual(v1.y, 1.0);
+		}
+		
+		// Test the division operator with a different type
+		TEST_METHOD(divisionOperatorDifferentType)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0 / 2;
+			Assert::AreEqual(v1.x, 0.5);
+			Assert::AreEqual(v1.y, 1.0);
+		}
+		
+		// Test the division by 0
+		TEST_METHOD(divisionByZero)
+		{
+			try
+			{
+				Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+				Aminophenol::Maths::Vector2<double> v1 = v0.divide(0.0);
+				Assert::Fail();
+			}
+			catch (std::exception e)
+			{
+				Assert::AreEqual(e.what(), "Cannot divide by zero.");
+			}
+		}
+
+		// Test the dot product method
+		TEST_METHOD(dotProductMethod)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(3.0, 4.0);
+			double dot = v0.dot(v1);
+			Assert::AreEqual(dot, 11.0);
+		}
+		
+		// Test the dot product operator
+		TEST_METHOD(dotProductOperator)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(3.0, 4.0);
+			double dot = v0 * v1;
+			Assert::AreEqual(dot, 11.0);
+		}
+
+		// Test the dot product operator with a different type
+		TEST_METHOD(dotProductOperatorDifferentType)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<int> v1(3, 4);
+			double dot = v0 * v1;
+			Assert::AreEqual(dot, 11.0);
+		}
+		
+		// Test the cross product method
+		TEST_METHOD(crossProductMethod)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(3.0, 4.0);
+			double cross = v0.cross(v1);
+			Assert::AreEqual(cross, -2.0);
+		}
+		
+		// Test the cross product operator
+		TEST_METHOD(crossProductOperator)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<double> v1(3.0, 4.0);
+			double cross = v0 ^ v1;
+			Assert::AreEqual(cross, -2.0);
+		}
+		
+		// Test the cross product operator with a different type
+		TEST_METHOD(crossProductOperatorDifferentType)
+		{
+			Aminophenol::Maths::Vector2<double> v0(1.0, 2.0);
+			Aminophenol::Maths::Vector2<int> v1(3, 4);
+			double cross = v0 ^ v1;
+			Assert::AreEqual(cross, -2.0);
+		}
+		
+		// Test the magnitude method
+		TEST_METHOD(magnitudeMethod)
+		{
+			Aminophenol::Maths::Vector2<double> v0(3.0, 4.0);
+			double mag = v0.magnitude();
+			Assert::AreEqual(mag, 5.0);
+		}
+		
+		// Test the normalise method
+		TEST_METHOD(normaliseMethod)
+		{
+			Aminophenol::Maths::Vector2<double> v0(3.0, 4.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0.normalize();
+			Assert::AreEqual(v1.x, 0.6);
+			Assert::AreEqual(v1.y, 0.8);
+		}
+		
+		// Test the normalise method with a magnitude of 0
+		TEST_METHOD(normaliseMethodZeroMagnitude)
+		{
+			Aminophenol::Maths::Vector2<double> v0(0.0, 0.0);
+			Aminophenol::Maths::Vector2<double> v1 = v0.normalize();
+			Assert::AreEqual(v1.x, 0.0);
+			Assert::AreEqual(v1.y, 0.0);
+		}
+		
 	};
 	
 }
