e9334504bd3601932fce256a9ecc7ab5615d24bd
Mathéo <matheo.galu56@gmail.com>
2023-08-08 20:16:57 +0200

    Input system axes and buttons accessors closes #14

--- Aminophenol/Input/InputSystem.h
+++ Aminophenol/Input/InputSystem.h
@@ -25,12 +25,39 @@ namespace Aminophenol
 		Maths::Vector2<double> getMousePosition();
 		void setCursorMode(CursorMode mode);
 
+		// Axis & buttons methods
+		void addAxis(
+			const std::string name,
+			const KeyCode keyNegative, const KeyCode keyPositive,
+			const float acceleration = 0.1f, const float deceleration = 0.1f
+		);
+		void addButton(
+			const std::string name, const KeyCode key
+		);
+		float getAxis(const std::string& name) const;
+		float getAxis(const Utils::UUID& uuid) const;
+		std::shared_ptr<InputAxis> getAxisPtr(const std::string& name) const;
+		std::shared_ptr<InputAxis> getAxisPtr(const Utils::UUID& uuid) const;
+		bool isButtonPressed(const std::string& name) const;
+		bool isButtonPressed(const Utils::UUID& uuid) const;
+		std::shared_ptr<InputButton> getButtonPtr(const std::string& name) const;
+		std::shared_ptr<InputButton> getButtonPtr(const Utils::UUID& uuid) const;
+		void removeAxis(const std::string& name);
+		void removeAxis(const Utils::UUID& uuid);
+		void removeButton(const std::string& name);
+		void removeButton(const Utils::UUID& uuid);
+
 	private:
 
 		const Window& m_window;
 
 		std::vector<std::shared_ptr<InputAxis>> m_axes;
 		std::vector<std::shared_ptr<InputButton>> m_buttons;
+
+		std::vector<std::shared_ptr<InputAxis>>::const_iterator findAxis(const std::string& name) const;
+		std::vector<std::shared_ptr<InputAxis>>::const_iterator findAxis(const Utils::UUID& uuid) const;
+		std::vector<std::shared_ptr<InputButton>>::const_iterator findButton(const std::string& name) const;
+		std::vector<std::shared_ptr<InputButton>>::const_iterator findButton(const Utils::UUID& uuid) const;
 
 	};
 
