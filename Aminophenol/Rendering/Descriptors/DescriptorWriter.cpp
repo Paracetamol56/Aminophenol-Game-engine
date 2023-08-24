f95e7e4c157e0f7caf48571f84e347a76e9eb098
Mathéo <matheo.galu56@gmail.com>
2023-08-24 14:40:13 +0200

    VkWriteDescriptorSet abstraction closes #43

---
+++ Aminophenol/Rendering/Descriptors/DescriptorWriter.cpp
@@ -0,0 +1,66 @@
+
+#include "pch.h"
+#include "DescriptorWriter.h"
+#include "DescriptorSetLayout.h"
+
+namespace Aminophenol
+{
+
+	DescriptorWriter::DescriptorWriter(DescriptorSetLayout& descriptorSetLayout, DescriptorPool& descriptorPool)
+		: m_descriptorSetLayout(descriptorSetLayout)
+		, m_descriptorPool(descriptorPool)
+	{}
+
+	DescriptorWriter& DescriptorWriter::writeBuffer(uint32_t binding, VkDescriptorBufferInfo* bufferInfo)
+	{
+		VkDescriptorSetLayoutBinding& bindingDescription = m_descriptorSetLayout.m_bindings[binding];
+
+		VkWriteDescriptorSet writeDescriptorSet = {};
+		writeDescriptorSet.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		writeDescriptorSet.dstBinding = binding;
+		writeDescriptorSet.descriptorType = bindingDescription.descriptorType;
+		writeDescriptorSet.descriptorCount = 1;
+		writeDescriptorSet.pBufferInfo = bufferInfo;
+
+		m_writeDescriptorSets.push_back(writeDescriptorSet);
+
+		return *this;
+	}
+
+	DescriptorWriter& DescriptorWriter::writeImage(uint32_t binding, VkDescriptorImageInfo* imageInfo)
+	{
+		VkDescriptorSetLayoutBinding& bindingDescription = m_descriptorSetLayout.m_bindings[binding];
+
+		VkWriteDescriptorSet writeDescriptorSet = {};
+		writeDescriptorSet.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		writeDescriptorSet.dstBinding = binding;
+		writeDescriptorSet.descriptorType = bindingDescription.descriptorType;
+		writeDescriptorSet.descriptorCount = 1;
+		writeDescriptorSet.pImageInfo = imageInfo;
+
+		m_writeDescriptorSets.push_back(writeDescriptorSet);
+
+		return *this;
+	}
+
+	void DescriptorWriter::build(VkDescriptorSet& descriptorSet)
+	{
+		m_descriptorPool.allocateDescriptorSet(m_descriptorSetLayout.getDescriptorSetLayout(), descriptorSet);
+		overwrite(descriptorSet);
+	}
+
+	void DescriptorWriter::overwrite(VkDescriptorSet& descriptorSet)
+	{
+		for (VkWriteDescriptorSet& writeDescriptorSet : m_writeDescriptorSets)
+			writeDescriptorSet.dstSet = descriptorSet;
+
+		vkUpdateDescriptorSets(
+			m_descriptorPool.m_logicalDevice,
+			static_cast<uint32_t>(m_writeDescriptorSets.size()),
+			m_writeDescriptorSets.data(),
+			0,
+			nullptr
+		);
+	}
+
+} // namespace Aminophenol
