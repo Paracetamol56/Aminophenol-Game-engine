43b55c5685d469a5f63619dba95e10f70d033906
Mathéo <matheo.galu56@gmail.com>
2023-08-24 14:47:52 +0200

    Add a Texture class with image loading

---
+++ Aminophenol/Rendering/Image/Texture.h
@@ -0,0 +1,31 @@
+
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "Image.h"
+
+namespace Aminophenol
+{
+
+	class Texture final : Image
+	{
+	public:
+
+		Texture(
+			const LogicalDevice& logicalDevice, const PhysicalDevice& physicalDevice, std::shared_ptr<CommandPool> commandPool,
+			std::filesystem::path filename, VkFilter filter = VK_FILTER_LINEAR, VkSamplerAddressMode addressMode = VK_SAMPLER_ADDRESS_MODE_REPEAT
+		);
+		~Texture();
+
+		std::filesystem::path getFilename() const;
+
+	private:
+
+		void load();
+		std::filesystem::path m_filemane;
+
+	};
+
+} // namespace Aminophenol
+
+#endif // !TEXTURE_H
