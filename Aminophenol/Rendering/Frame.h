c11a2c604359a834276a31874b41a5a73f3e3ef2
Mathéo <matheo.galu56@gmail.com>
2023-08-24 14:41:50 +0200

    Add UBO data and UBO buffer to the frame struct

--- Aminophenol/Rendering/Frame.h
+++ Aminophenol/Rendering/Frame.h
@@ -2,13 +2,25 @@
 #ifndef FRAME_H
 #define FRAME_H
 
+#include "Utils/NonCopyable.h"
+#include "Rendering/Buffers/UniformBuffer.h"
 #include "Rendering/Commands/CommandBuffer.h"
 #include "Rendering/Image/ImageDepth.h"
+#include "Rendering/Pipeline/Pipeline.h"
 
 namespace Aminophenol
 {
 
-	struct Frame {
+	struct FrameUniformBufferObject
+	{
+		Maths::Matrix4f projectionMatrix;
+		Maths::Matrix4f viewMatrix;
+		Maths::Matrix4f modelMatrix;
+		Maths::Matrix4f normalMatrix;
+	};
+
+	struct Frame
+	{
 
 		// Frame size
 		uint32_t width;
@@ -22,6 +34,13 @@ namespace Aminophenol
 
 		// Command buffer
 		std::unique_ptr<CommandBuffer> commandBuffer;
+
+		// Frame UBO
+		FrameUniformBufferObject uniformBufferData;
+		std::unique_ptr<UniformBuffer> uniformBuffer;
+
+		// Attachment
+		VkImageView attachment;
 
 		// Syncronization objects
 		VkSemaphore imageAvailableSemaphore;
